ced4093a72caea4b372a41b57a491171aa87c80b
max <stockm0819@gmail.com>
2024-04-11 12:18:04 -0400

    feature/simplify-animal-components: resolved useEffect dependency issue and updated readme

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 The purpose of this application is to provide quick and humorous enteriainment for all animal lovers. I hope this web application provides value to you and your friends.
 
 # How to Run
-    Enter the following into your terminal. Ensure that you are located in the application root folder prior to running commands 
+Enter the following into your terminal. Ensure that you are located in the application root folder prior to running commands. 
 ### `npm install`
 ### `npm start`
 
--- src/AnimalCardComponent.js
+++ src/AnimalCardComponent.js
@@ -1,22 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Button, Center, Image } from '@chakra-ui/react'
 import FetchData  from './AnimalCardComponentUtils';
 
-
-
 function AnimalCardComponent({name}) {
-  const [data, setData] = useState([FetchData()]); 
+  const [data, setData] = useState([]); 
   
-  let result = async () => {
-    let dataum = await FetchData(name)
-    setData(dataum);
-    return dataum;
-  }
-  useEffect( () => {
-    setData(result());
-  }, []); 
+  const changePhoto = useCallback( async () => {
+      let payload = await FetchData(name)
+      setData(payload);      
+      return payload;
+  }, [name]);
 
-  
+  useEffect( () => {
+     changePhoto()
+  }, [changePhoto]); 
+
 
   return (
     <div>
@@ -25,12 +23,12 @@ function AnimalCardComponent({name}) {
           {data.map(item => (
             <>
                 <Center>
-                  <Image borderRadius='lg' boxSize='540px' src={item.url} alt='cat' />
+                  <Image borderRadius='lg' boxSize='640px' src={item.url} alt='cat' />
                   </Center>
             <br/><p>
                 Source: {item.url}
               </p>
-              <Button onClick={result}>New {name}</Button>
+              <Button onClick={changePhoto}>New {name}</Button>
               
             </>
             
