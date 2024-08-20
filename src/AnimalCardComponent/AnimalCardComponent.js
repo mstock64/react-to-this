5cff66373f0590960440cb5b90ebf8c84522e9fc
max <stockm0819@gmail.com>
2024-08-20 10:42:33 -0400

    Updated AnimalCardComponent - Now leverages tailwind for UI elements - Charkia UI has been removed entirely - Functionility added but requires more testing going back to old animal photos
    HomeComponent
    - New UI Bar introduced
    - New Starting text has been introduced to reduce user confusion
    - All refs to Charkia UI has been removed
    
    App
    - All Refs to charkia UI has been removed

--- src/AnimalCardComponent/AnimalCardComponent.js
+++ src/AnimalCardComponent/AnimalCardComponent.js
@@ -1,37 +1,96 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { Button, Center, Image } from '@chakra-ui/react'
-import FetchData  from './AnimalCardComponentUtils';
-
-function AnimalCardComponent({name}) {
-  const [data, setData] = useState([]); 
-  
-  const changePhoto = useCallback( async () => {
-      let payload = await FetchData(name)
-      setData(payload);      
-      return payload;
-  }, [name]);
-
-  useEffect( () => {
-     changePhoto()
-  }, [changePhoto]); 
+import FetchData from './AnimalCardComponentUtils';
 
 
+function AnimalCardComponent({ name = 'cat', state = false }) {
+
+  const [data, setData] = useState([]);
+  const [previousData, setPreviousData] = useState([])
+  const [forwardData, setForwardData] = useState([])
+
+  const changePhoto = useCallback(async () => {
+    let payload = await FetchData(name)
+    
+    let previous = previousData
+    previous.push(payload)
+
+    setPreviousData(previous)
+    setData(payload);
+
+    console.log(previousData.length)
+    logIt()
+  });
+
+  useEffect(() => {
+    changePhoto();
+
+  }, [state])
+
+  const logIt = () => {
+    console.log(previousData)
+    console.log(forwardData)
+  }
+  const goBack = useCallback(() => {
+
+    let previous = previousData
+    let forward = forwardData
+    if(previous.length < 1)
+      return;
+    let curr = previous.pop()
+    forward.push(data)
+
+    setData(curr)
+    setPreviousData(previous)
+    setForwardData(forward)
+    logIt()
+    
+  });
+  const goForward = useCallback(() => {
+    let previous = previousData
+    let forward = forwardData
+    
+    if(forward.length < 1)
+      return;
+    
+    let curr = forward.pop()
+    previous.push(data)
+
+    setData(curr)
+    setPreviousData(previous)
+    setForwardData(forward)
+    logIt() 
+  });
+  const transformstate = () => {
+    if(forwardData.length > 0)
+      goForward()
+    else
+      changePhoto()
+  }
   return (
     <div>
-      {data.length > 0 ? (
+      {data !== undefined && data.length > 0 ? (
         <span>
           {data.map(item => (
             <>
-                <Center>
-                  <Image borderRadius='lg' boxSize='640px' src={item.url} alt='cat' />
-                  </Center>
-            <br/><p>
+              
+              <div class='mx-auto ml-2 mr-2'>
+                <img src={item.url} class='rounded-lg mx-auto shadow-xl' alt={name} />
+              </div>
+
+              <br /><p class='text-sm'>
                 Source: {item.url}
               </p>
-              <Button onClick={changePhoto}>New {name}</Button>
+
+              {previousData.length > 1 ? (
+                
+                <div class='m-3 inline-flex'> 
+                  <button onClick={goBack} class='rounded-md bg-gray-800 text-white text-xl p-2'> Back</button> 
+                  </div>) : ('')}
+
+              <button onClick={transformstate} class='rounded-md bg-gray-800 text-white text-xl p-2'>Next</button>
               
             </>
-            
+
           ))}
 
         </span>
