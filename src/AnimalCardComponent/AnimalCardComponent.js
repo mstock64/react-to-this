8b73708b46e2ae5d3688129104b6a53e9f683d33
max <stockm0819@gmail.com>
2024-10-03 17:03:45 -0400

    Added Changes to button placement

--- src/AnimalCardComponent/AnimalCardComponent.js
+++ src/AnimalCardComponent/AnimalCardComponent.js
@@ -1,32 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import FetchData from './AnimalCardComponentUtils';
-
 
 function AnimalCardComponent({ name = 'cat', state = false }) {
 
-  const [data, setData] = useState([]);
   const [previousData, setPreviousData] = useState([])
   const [forwardData, setForwardData] = useState([])
-
-  const changePhoto = async () => {
+  const changePhoto = useCallback(async () => {
     let payload = await FetchData(name)
-    
+
     let previous = previousData
     previous.push(payload)
 
     setPreviousData(previous)
     setData(payload);
+  });
 
-    console.log(previousData.length)
-  };
+  const [data, setData] = useState(changePhoto);
 
- 
 
-  const goBack = () => {
+  const goBack = useCallback(() => {
 
     let previous = previousData
     let forward = forwardData
-    if(previous.length < 1)
+    if (previous.length < 1)
       return;
     let curr = previous.pop()
     forward.push(data)
@@ -34,68 +30,72 @@ function AnimalCardComponent({ name = 'cat', state = false }) {
     setData(curr)
     setPreviousData(previous)
     setForwardData(forward)
-    
-  };
-  const goForward = () => {
+
+  });
+  const goForward = useCallback(() => {
     let previous = previousData
     let forward = forwardData
-    
-    if(forward.length < 1)
+
+    if (forward.length < 1)
       return;
-    
+
     let curr = forward.pop()
     previous.push(data)
 
     setData(curr)
     setPreviousData(previous)
     setForwardData(forward)
-  };
+
+  });
   const transformstate = () => {
-    if(forwardData.length > 0)
+    if (forwardData.length > 0)
       goForward()
     else
       changePhoto()
   }
   return (
-    <div>
+    <div className=' '>
       {data !== undefined && data.length > 0 ? (
-        <span>
+        <>
           {data.map(item => (
             <>
-              
-              <div class='mx-auto ml-2 mr-2'>
-                <img src={item.url} class='rounded-lg mx-auto shadow-xl' alt={name} />
+              <div className='grid grid-cols-1 gap-y-4 fixed'>
+                <div class='mx-auto ml-2 mr-2 '>
+                <div className='w-screen h-screen overflow-hidden overflow-x-hidden'>
+                  <div className='w-fit mx-auto relative'>
+                    <img src={item.url} class='rounded-lg mx-auto shadow-xl' alt={name} />
+                    <div className='opacity-0'>
+                      {previousData.length > 1 ? (
+                        <>
+                        <button onClick={transformstate} class='rounded-r-md bg-blue-400  text-white text-xl p-2 z-50 absolute top-0 right-0 h-full w-1/2'></button>
+
+                      < button onClick={goBack} class='rounded-l-md bg-blue-400 text-white text-xl border-r-2 border-black  p-2 z-50 absolute top-0 left-0 h-full w-1/2 '>  </button>
+                      </>
+                      ) : (
+                        < button onClick={transformstate} class='rounded-l-md bg-blue-400 text-white text-xl border-r-2 border-black  p-2 z-50 absolute top-0 left-0 h-full w-full'>  </button>)}
+                        
+                  </div>
+
+                  </div>
+                </div>
+
+
               </div>
 
-              <br /><p class='text-sm'>
-                Source: {item.url}
-              </p>
-
-              {previousData.length > 1 ? (
-                
-                <div class='m-3 inline-flex'> 
-                  <button onClick={goBack} class='rounded-md bg-gray-800 text-white text-xl p-2'> Back</button> 
-                  </div>) : ('')}
-
-              <button onClick={transformstate} class='rounded-md bg-gray-800 text-white text-xl p-2'>Next</button>
-              
+            </div >
             </>
 
-          ))}
+      ))}
+    </>
 
-        </span>
-      ) : (
-        <div>
-        <p>Loading data...</p>
-        {setTimeout(() => {
-          changePhoto()
-        }, 30)}
-        </div>
-      
-      )}
-    </div>
+  ) : (
+    <p>Loading data...</p>
+  )
+}
+    </div >
   );
 }
 
 export default AnimalCardComponent;
+
 
